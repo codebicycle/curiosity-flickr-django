0f5c9d4c20c0c897fd7c1c69817c2ccba8d7a15d
codebicycle <codebicycle@gmail.com>
2017-07-31 15:34:34 +0300

    Update URLs

--- config/urls.py
+++ config/urls.py
@@ -19,8 +19,9 @@ from django.contrib import admin
 import flickr.views
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^$', flickr.views.Interestingness.as_view()),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', flickr.views.PeopleView.as_view()),
+    url(r'^interesting/$', flickr.views.Interestingness.as_view()),
     url(r'^people/$', flickr.views.PeopleView.as_view(), name='people'),
     # url(r'^people/fav/$', flickr.views.FavView.as_view(), name='fav'),
 
